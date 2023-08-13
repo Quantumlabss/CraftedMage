8bda960362d474baefffe0d64e745204f9a0bad4
Cubicl18 <alex.spadyevans@icloud.com>
2023-08-13 13:31:40 -0600

    first push

---
+++ src/main/java/org/crafted/craftedmage/craftedmage/CraftedMage.java
@@ -0,0 +1,17 @@
+package org.crafted.craftedmage.craftedmage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftedMage extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
